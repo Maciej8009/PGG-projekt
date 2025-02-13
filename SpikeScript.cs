8fe82d50d1ea40db243d813f921853dbad8fdc55
Maciej Kowalik <100430330+Maciej8009@users.noreply.github.com>
2025-02-13 02:28:51 +0100

    pgg

---
+++ SpikeScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour
+{
+    public SpikeGenerator spikeGenerator;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * spikeGenerator.CurrentSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("nextLine")) 
+            {
+                spikeGenerator.GenerateNextSpikeWithGap();
+            }
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
